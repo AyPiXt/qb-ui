2d3f0ab390b41a4a825e4a60077295657ee4771c
AyPi <76070305+AyPiXt@users.noreply.github.com>
2021-12-20 22:09:25 +0100

    updated stuff

--- client.lua
+++ client.lua
@@ -1,19 +1,3 @@
-RegisterNetEvent('qb-ui:ShowUI')
-AddEventHandler('qb-ui:ShowUI', function(action, text)
-	SendNUIMessage({
-        type = "open",
-        text = text,
-        color = type,
-    })
-end)
-
-RegisterNetEvent('qb-ui:HideUI')
-AddEventHandler('qb-ui:HideUI', function()
-	SendNUIMessage({
-		type = "close",
-	})
-end)
-
 function showInteraction(text , type)
     SendNUIMessage({
         type = "open",
